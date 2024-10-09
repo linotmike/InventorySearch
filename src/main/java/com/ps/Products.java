406f1f3db2f2252c68991964282dca3e6eb03b0c
linotmike <128604219+linotmike@users.noreply.github.com>
2024-10-09 10:32:06 -0700

    Made properties for Product class

---
+++ src/main/java/com/ps/Products.java
@@ -0,0 +1,13 @@
+package com.ps;
+
+public class Products {
+    private int id;
+    private String name;
+    private float price;
+    private String category;
+    private int quantity;
+    private String description;
+
+
+
+}
