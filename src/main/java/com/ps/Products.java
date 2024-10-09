d9c8302b18c635541bf7e1b935e60052c448293c
linotmike <128604219+linotmike@users.noreply.github.com>
2024-10-09 10:34:46 -0700

    Added getters and setters

--- src/main/java/com/ps/Products.java
+++ src/main/java/com/ps/Products.java
@@ -20,5 +20,51 @@ public class Products {
         this.description = description;
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
